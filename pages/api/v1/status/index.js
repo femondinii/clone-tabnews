db9704350a471df8e1670f73aa4dcc1444fb00cf
Luis Felipe Mondini <105131541+femondinii@users.noreply.github.com>
2026-02-24 00:49:20 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from 'infra/database.js';
 
 async function status(req, res) {
-  const result = await database.query('SELECT 1 + 1;');
-  console.log(result.rows);
-  res.status(200).json({ status: 'ok' });
+  const updatedAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  console.log(databaseOpenedConnections.rows)
+  res.status(200).json({
+    update_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(databaseMaxConnections.rows[0].max_connections),
+        opened_connections: databaseOpenedConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
